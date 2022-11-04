748390fd0cfed5efccaaff3bff03d7cb06fa128a
Ben Hanzl <ben.hanzl@gmail.com>
2022-11-03 22:24:37 -0400

    Added QR Code page

---
+++ pages/qr-code.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function QrCode() {
+  return (
+    <div className="bg-zinc-100 h-screen flex items-center justify-center">
+      <article className="bg-white w-80 p-4 rounded-2xl">
+        <Image
+          src="/image-qr-code.png"
+          alt="QR Code"
+          width={288}
+          height={288}
+          className="rounded-lg mb-6"
+        />
+        <section className="flex flex-col space-y-4 pb-6">
+          <h1 className="text-center text-slate-800 text-xl font-extrabold">
+            Improve your front-end skills by building projects
+          </h1>
+          <p className="text-center text-slate-500 text-sm font-extralight">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </section>
+      </article>
+    </div>
+  );
+}
