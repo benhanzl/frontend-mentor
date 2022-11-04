acbb5f18036ff77f5882e05f8d84d6e19135e660
Ben Hanzl <ben.hanzl@gmail.com>
2022-11-03 21:44:44 -0400

    Removed generated HTML and CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function FrontendMentor({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default FrontendMentor;
